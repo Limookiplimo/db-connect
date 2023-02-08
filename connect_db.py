f0ec9098f69ec30cc7b31213b4b6fa9c549bdc32
Limoo Kiplimo <83447578+Limookiplimo@users.noreply.github.com>
2023-02-08 14:11:53 +0300

    Update connect_db.py
    Updated db connection

--- connect_db.py
+++ connect_db.py
@@ -11,7 +11,7 @@ for record in range(1000):
 
 db_data = tuple(records)
 
-conn_string = "dbname='testdb' host='host.docker.internal' user='user' password='admin123'"
+conn_string = "dbname='testdb' host='localhost' user='' password=''"
 conn = db.connect(conn_string)
 cur = conn.cursor()
 
